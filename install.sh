e76f4ecdf9bf189928e270c57a63b25edb9fc9e0
hieuvu1998 <88468065+hieuvu1998@users.noreply.github.com>
2023-03-04 23:11:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,6 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/hieuvu1998/proxyv6/main/3proxy-0.9.4.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
-    make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
